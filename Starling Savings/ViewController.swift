2ee3dc80425fc47ed0e22a96b6c5888f3b5b1ecf
Edward Danescu <edward.danescu@gmail.com>
2020-02-06 16:15:39 +0000

    Initial Commit

---
+++ Starling Savings/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Starling Savings
+//
+//  Created by Edward on 06/02/2020.
+//  Copyright © 2020 Edward Theodor Danescu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
